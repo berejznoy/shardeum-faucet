141a383b71af0193d77b1b63f531072a0a6fe872
berezhnoi <S32069257b!!!>
2023-03-15 14:13:43 +0300

    refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,8 @@ import {redisStore} from 'cache-manager-redis-store';
 @Module({
   imports: [CacheModule.register({
     store: redisStore as any,
-    host: 'redis-16445.c11.us-east-1-2.ec2.cloud.redislabs.com',
-    port: 16445
+    url: 'redis://skaarj:S32069257b!@redis-16445.c11.us-east-1-2.ec2.cloud.redislabs.com:16445',
+    isGlobal: true
   })],
   controllers: [AppController],
   providers: [AppService],
