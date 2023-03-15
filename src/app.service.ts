c5f34068966aa52f660f6f830bf1763c8109e716
berezhnoi <S32069257b!!!>
2023-03-15 10:57:51 +0300

    change cache

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
 import {CACHE_MANAGER, Inject, Injectable} from "@nestjs/common";
-import { ethers, Signer } from "ethers";
-import { Cache } from 'cache-manager';
+import {ethers, Signer} from "ethers";
+import {Cache} from 'cache-manager';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require("dotenv").config();
@@ -15,6 +15,8 @@ export class AppService {
   // init
   provider: ethers.providers.Provider;
   signer: Signer;
+  timeout: { [address: string]: number } = { '0x9482D18c937ddB9D9b85697c9b31A8032F9f8712': 1673849821252 };
+
   constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {
     // 2. Define network configurations
     const providerRPC = {
@@ -48,6 +50,13 @@ export class AppService {
       return { success: false, message: "Invalid address" };
     }
 
+    if (await this.cacheManager.get(_address)) {
+      return {
+        success: false,
+        message: "Please wait for 12 hours to claim again",
+      };
+    }
+
     const balance = await this.signer.getBalance().catch((err) => {
       return { success: false, message: "RPC Error. Try again" };
       throw new Error(err);
@@ -57,25 +66,18 @@ export class AppService {
       return { success: false, message: "Faucet is empty. Try again" };
       throw new Error("Faucet is empty. Try again");
     }
-
-    try {
-      if (await this.cacheManager.get(_address)) {
-        return {
-          success: false,
-          message: "Please wait for 12 hours to claim again",
-        };
-      }
-      else {
-        const res = await this.signer.sendTransaction({
+    let res = await this.signer
+        .sendTransaction({
           to: _address,
           value: ethers.utils.parseEther("1"),
+        })
+        .then((tx) => {
+          this.timeout[_address] = (Date.now() as number) + 43200000;
+          return { success: true, message: tx.hash };
+        })
+        .catch((err) => {
+          return { success: false, message: 'RPC Error. Try again' };
+          throw new Error(err);
         });
-        await this.cacheManager.set(_address, true, 43200);
-        return {success: true, message: res.hash};
-      }
-    } catch (e) {
-      return { success: false, message: "RPC Error. Try again" };
-      throw new Error(e);
-    }
   }
 }
