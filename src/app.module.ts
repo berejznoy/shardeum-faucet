a7cbb09a57ddc08cfa5fd830bf16118a9a1c2aec
berezhnoi <S32069257b!!!>
2023-03-15 10:28:55 +0300

    change cache manager to redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import {redisStore} from 'cache-manager-redis-store';
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [CacheModule.register({
+    store: redisStore as any,
+    host: 'redis-16445.c11.us-east-1-2.ec2.cloud.redislabs.com',
+    port: 16445
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
