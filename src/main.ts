bb8fc1347305b8ad1fbe5cab29a7bce2b5daea5d
berezhnoi <S32069257b!!!>
2023-03-29 20:53:20 +0300

    added balance endpoint

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("docs", app, document);
-  //app.enableCors();
+  app.enableCors();
   await app.listen(3000);
 
   if (module.hot) {
