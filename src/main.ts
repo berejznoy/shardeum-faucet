38d21c1e39f9afbbb5ecaa337a5d40c56678a575
berezhnoi <S32069257b!!!>
2023-03-29 21:01:34 +0300

    added balance endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,41 @@
-import { NestFactory } from "@nestjs/core";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-import { AppModule } from "./app.module";
+import {NestFactory} from "@nestjs/core";
+import {SwaggerModule, DocumentBuilder} from "@nestjs/swagger";
+import {AppModule} from "./app.module";
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const corsOptions = {
-    origin: true,
-    methods: "POST",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-    allowedHeaders: "Content-Type, Accept, Authorization",
-  };
-  app.enableCors(corsOptions);
+    const app = await NestFactory.create(AppModule);
+    const corsOptions = {
+        origin: true,
+        methods: "POST",
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        credentials: true,
+        allowedHeaders: "Content-Type, Accept, Authorization",
+    };
+    app.enableCors(corsOptions);
 
-  const config = new DocumentBuilder()
-    .setTitle("Capstone Team 4 Encode Bootcamp")
-    .setDescription("API description for Capstone Team 4 Encode")
-    .setVersion("1.0")
-    .addTag("Capstone Project")
-    .build();
+    const config = new DocumentBuilder()
+        .setTitle("Capstone Team 4 Encode Bootcamp")
+        .setDescription("API description for Capstone Team 4 Encode")
+        .setVersion("1.0")
+        .addTag("Capstone Project")
+        .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("docs", app, document);
-  app.enableCors();
-  await app.listen(3000);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("docs", app, document);
+    app.enableCors({
+        allowedHeaders: '*',
+        origin: '*',
+        credentials: true,
+    });
+    await app.listen(3000);
 
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 }
+
 bootstrap();
